2aa450629d4c26bc274876cdbb284c4797086c9e
Shantanu <shantanu_negi@hotmail.com>
2023-09-27 15:43:19 +1000

    added localstorage database.

--- LiveHealthChecks.UI/Program.cs
+++ LiveHealthChecks.UI/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using LiveHealthChecks.UI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
