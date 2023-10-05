91eacabf4f8f0e423d11e987b2339c644fa9078f
Shantanu <shantanu_negi@hotmail.com>
2023-10-05 15:47:58 +1100

    moved models to models folder.

--- LiveHealthChecks.UI/Models/Models.cs
+++ LiveHealthChecks.UI/Models/Models.cs
@@ -1,5 +1,27 @@
 ﻿namespace LiveHealthChecks.UI.Models
 {
+    public class MyHealthCheckAuthModel
+    {
+        public string ClientId { get; set; } = string.Empty;
+        public string ReceiveMethod { get; set; } = string.Empty;
+        public string SecretKey { get; set; } = string.Empty;
+    }
+
+    public class MyApiHealthCheckModel
+    {
+        public string ApiName { get; set; } = string.Empty;
+        public string ReceiveMethod { get; set; } = string.Empty;
+    }
+
+    public class DashboardSettings
+    {
+        public string ServerUrl { get; set; } = string.Empty;
+        public string ServerReceiveMethod { get; set; } = string.Empty;
+        public string ServerSecretKey { get; set; } = string.Empty;
+        public string ServerClientId { get; set; } = string.Empty;
+        public List<MyApiHealthCheckModel> Apis { get; set; } = new List<MyApiHealthCheckModel>();
+    }
+
     public class HealthCheck
     {
         public string Api { get; set; } = string.Empty;
